177fbf052f2bd62bfe9d856fcac45c1df25c36b9
Collins Kandongwe <kandongwec@berea.edu>
2021-09-13 03:36:42 -0400

    added a function to allow smooth scrolling from one section to another

--- javascript/script.js
+++ javascript/script.js
@@ -5,9 +5,22 @@ $(document).ready(function() {
         $('header').toggleClass('toggle');
     });
 
-    $(window).on('scroll load', function(){
+    $(window).on('scroll load', function() {
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
 
     });
+
+    // smooth scrolling
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top,
+            },
+            500,
+            'linear'
+        );
+    });
+
+
 });
